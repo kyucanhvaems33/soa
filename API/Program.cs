dd6b15ec4c3915757c58ca744dc9418c72d27881
ThangBuiDuc <thangbd@hpu.edu.vn>
2022-10-08 16:48:52 +0700

    update

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCors(p => p.AddPolicy("corspolicy", build =>
+{
+    build.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
 
 var app = builder.Build();
 
